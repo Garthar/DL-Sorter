4c333eb0b51cfd82feb3c91047e76633bf2bb5fb
Oliprik <oliprik121@gmail.com>
2017-12-13 14:57:35 +0000

    Added couple of TODO'S

--- DL-Sorter.py
+++ DL-Sorter.py
@@ -3,6 +3,7 @@ import argparse
 import re
 from pathlib import Path
 import glob
+import os
 
 parser = argparse.ArgumentParser()
 parser.add_argument("src", help="Source directory")
@@ -17,9 +18,24 @@ def pirateHelper(byrjun, endir):
 
     for show in src:
         if not show.is_dir():
-            #print(show.suffix)
-            nyttSett.add(show.suffix)   #addar unique endingum inn í settið nyttSett
-    print(nyttSett)
+            print(show)
+            #nyttSett.add(show.suffix)   #addar unique endingum inn í settið nyttSett
 
+            #TODO Checka hvort þáttur sé örugglega movie fæll og ef það er movie fæll þá hendir hann í get info og movie
+            if True:
+                info = getInfo(show)
+                #moveFile(show) færum fælinn
+    #print(nyttSett)
 
+def getInfo(show):
+    return None
+    #TODO
+    #Get show name, episode nr og season.
+    #Ef fællinn er ekki lýsandi á episode nr og season nr þá þurfum við að checka parent folder alveg upp að rótinni á download möppunni.
+    #returna nafni, episode nr. og season nr.
+    #Ef þetta er ekki þáttur heldur bíómynd eða annað þá skilum við bara episode eða season nr -1 og checkum á því í pirateHelper og sleppum því að færa hann.
+def moveFile(nafn, episode, season, dest):
+    #TODO
+    #Notum þetta til að færa fælinn í destination og eyða gamla þegar við erum búnir að finna allar upplýsingar
+    return None
 pirateHelper(args.src, args.dst)
---
+++ run.bat
@@ -0,0 +1,3 @@
+python DL-Sorter.py C:\Users\oliprik\Desktop\python\pirate\downloads\downloads C:\Users\oliprik\Desktop\python\pirate\target\
+
+PAUSE
