7d1b6c278ee56081793309d2226ec939b163b810
Oliprik <oliprik121@gmail.com>
2017-12-13 22:16:13 +0000

    Added first version of getting season number

--- DL-Sorter.py
+++ DL-Sorter.py
@@ -10,25 +10,37 @@ parser.add_argument("src", help="Source directory")
 parser.add_argument("dst", help="Destination directory")
 args = parser.parse_args()
 
-
+allowedSuffixes = {'.avi', '.flv','.wmv', '.mov', '.divx', '.mp4', '.mta',  '.mpg', '.m4v','.smi', '.mkv','.sfv', '.AVI'}
 
 def pirateHelper(byrjun, endir):
     src = Path(byrjun).glob('**/*') #fer recursively í gegnum allar möppur í undir current path
-    nyttSett = set()
+
 
     for show in src:
         if not show.is_dir():
-            print(show)
-            #nyttSett.add(show.suffix)   #addar unique endingum inn í settið nyttSett
+            #print(show)
 
-            #TODO Checka hvort þáttur sé örugglega movie fæll og ef það er movie fæll þá hendir hann í get info og movie
-            if True:
+            #Checka hvort þáttur sé örugglega movie fæll og ef það er movie fæll þá hendir hann í get info og ef þetta er þáttur þá moveFile
+            if show.suffix in allowedSuffixes:
+
                 info = getInfo(show)
-                #moveFile(show) færum fælinn
-    #print(nyttSett)
+                print(info)
+                if True == True: #Checkum hvort season nr. sé ekki -1 því ef það er -1 þá er það ekki þáttur
+                    #moveFile(show, info) færum fælinn
+                    b = 1 #Bara svo forritið keyri án þess að hafa klára if statementið
+
 
 def getInfo(show):
-    return None
+    season = -1
+    name = 'notfound'
+    #print(os.path.basename(show))
+
+    b = re.search('((S|s)[0-9]+)', str(show)) #Sækjum season númerinu. Þetta virkar á sirka helminginn af þáttunum
+    if b != None:
+        season = b.string[b.start()+1:b.end()] #Breytir search resultinu í string (+1 til að sleppa S-inu)
+    season = str(season) #Breytum í string svo hægt sé að skera 0 framan af tölunni auðveldlega
+    season = season.lstrip('0')    # Þetta tekur 0-ið sem kemur fremst á töluna.
+    return int(season) #Skilum tölunni sem integer.
     #TODO
     #Get show name, episode nr og season.
     #Ef fællinn er ekki lýsandi á episode nr og season nr þá þurfum við að checka parent folder alveg upp að rótinni á download möppunni.
