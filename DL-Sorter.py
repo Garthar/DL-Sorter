b0a3b3f3a49a1dcb092b42ed21c7c224fef822d8
Garthar <gardar14@ru.is>
2017-12-15 00:28:27 +0000

    move function added and some regex-name modifications

--- DL-Sorter.py
+++ DL-Sorter.py
@@ -1,17 +1,15 @@
-import shutil
+from shutil import copy
+from pathlib import Path
 import argparse
 import re
-from pathlib import Path
-import glob
-import os
-import hashlib
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("src", help="Source directory")
 parser.add_argument("dst", help="Destination directory")
 args = parser.parse_args()
 
-allowedSuffixes = {'.avi', '.flv', '.wmv', '.mov', '.divx', '.mp4', '.mta',  '.mpg', '.m4v','.smi', '.mkv', '.sfv', '.AVI'}
+allowedSuffixes = {'.avi', '.flv', '.wmv', '.mov', '.divx', '.mp4', '.mta',  '.mpg', '.m4v', '.smi', '.mkv', '.AVI'}
 
 def pirateHelper(byrjun, endir):
     src = Path(byrjun).glob('**/*') #fer recursively í gegnum allar möppur í undir current path
@@ -23,49 +21,30 @@ def pirateHelper(byrjun, endir):
                 test = re.search(r'.*[\/\\](.*)[sS](\d{1,2})', str(show))
                 if test:
                     show_name = re.sub(r'\'', '',test.group(1))
-                    show_name = re.sub(r'\W', ' ',show_name).strip().title()
+                    show_name = re.sub(r'[\. _-]+', ' ',show_name).strip().title()
+                    if not show_name:
+                        temp = str(show).split('\\')[:-1]
+                        #print(temp[::-1])
+                        for t in temp[::-1]:
+                            xname = re.sub(r'[sS]\d{1,2}|[sS]eason[\.-_ ]*\d{1,2}', '', t)
+                            if xname:
+                                show_name = xname
+                                show_name = re.sub(r'\'', '', show_name)
+                                show_name = re.sub(r'[\. _-]+', ' ', show_name).strip().title()
+                                break
+
                     season_number = 'Season ' + test.group(2).zfill(2)
-                    print(Path(endir, show_name, season_number))
-                info = getInfo(show)
-                if info > -1: #Checkum hvort season nr. sé ekki -1 því ef það er -1 þá er það ekki þáttur
-                    #Hér kemur move file á file-inn
-                    #print("Season: "+str(info)+" - "+str(show))
-                    pass
+
+                    dest_path = Path(endir, show_name, season_number)
+                    if not dest_path.exists():
+                        dest_path.mkdir(parents=True)
+                    copy(show, dest_path)
 
 
 def getInfo(show):
-    season = -1
-    name = 'notfound'
-    #print(os.path.basename(show))
-    #name = re.search('^(.*?)s[0-9]', str(show))
-    name = re.search(r'.*[\/\\](.+?)[sS]\d{1,2}[eE]\d{1,3}\.\w+$', str(show))
-    #if name:
-#        print(name.group(1))
-    #print(name)
-    b = re.search('((S|s)[0-9]+)', str(show)) #Sækjum season númerinu. Þetta virkar á sirka helminginn af þáttunum
-    if b != None:
-        season = b.string[b.start()+1:b.end()] #Breytir search resultinu í string (+1 til að sleppa S-inu)
-    season = str(season) #Breytum í string svo hægt sé að skera 0 framan af tölunni auðveldlega
-    season = season.lstrip('0')    # Þetta tekur 0-ið sem kemur fremst á töluna.
-    return int(season) #Skilum tölunni sem (SKilum líka nafn hér)
+    return 0
 
 def moveFile(show, season, dest):
-    #TODO
-    #Notum þetta til að færa fælinn í destination og eyða gamla þegar við erum búnir að finna allar upplýsingar
-    #dlfile = os.listdir(r"C:\\Users\\oliprik\\Desktop\\python\\pirate\\downloads")
-    dlfile = show
-    for file in dlfile:
-        #print(file)
-        if os.path.isdir(dlfile + '/' + file):
-            continue
-        else:
-            if os.path.exists(dest + '/' + file):
-                Data = open(dlfile + file, "r").read()
-                m = hashlib.sha1()
-                m.update(Data)
-                h = (m.hexdigest())[0:5]
-                file(dest + "W").write(Data)
-            print (file)
-    #print (episode,season)
-    return None
+    return 0
+
 pirateHelper(args.src, args.dst)
